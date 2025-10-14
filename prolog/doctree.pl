5899fbc75768b2584242edcfa87f6f7e0e91119f
EmilioGiordano <giordanoemilio21@gmail.com>
2025-10-14 00:12:06 -0300

    Fix weird text symbols generated on html

--- prolog/doctree.pl
+++ prolog/doctree.pl
@@ -7,7 +7,6 @@
 main(Facts, Out) :-
   load_facts(Facts),
   open(Out, write, S, [encoding(utf8)]),
-  put_code(S, 0xFEFF),
   render_html(S),
   close(S).
 
@@ -20,15 +19,12 @@ load_facts(File) :-
 render_html(S) :-
   format(S, "<!doctype html><meta charset=\"utf-8\"><ul>~n", []),
 
-
   forall(pre_root_module(Id), node_li(S, Id)),
 
-  % 2) routers raíz (no referenciados como rid/1)
   forall( (router(Rid, Type, Name, Attrs), \+ appears_in_any_path_router(Rid)),
           router_li(S, Rid, Type, Name, Attrs) ),
 
   format(S, "</ul>~n", []).
-
 
 pre_root_module(Id) :-
   module(Id, _, _, _),
@@ -44,41 +40,34 @@ appears_in_any_path_router(Rid) :-
 
 has_paths(Rid) :- path(Rid, _, _, _).
 
-
 node_li(S, Id) :-
   module(Id, ModType, Name, _),
   nz(Name, Nice),
-  format(S, "<li>[~w] ~w — <code>~w</code></li>~n", [Id, Nice, ModType]).
+  format(S, "<li>[~w] ~w - <code>~w</code></li>~n", [Id, Nice, ModType]).
 
 router_li(S, Rid, Type, Name, _Attrs) :-
   nz(Name, Nice),
-  % abrimos el <li> del router
   format(S, "<li>ROUTER [~w] ~w", [Rid, Nice]),
   ( has_paths(Rid) ->
-      % hijos (rutas) DENTRO del mismo <li>
       format(S, "<ul>~n", []),
       render_router_paths(S, Rid),
       format(S, "</ul>", [])
     ; true ),
-  % cerramos el <li> del router
   format(S, "</li>~n", []).
 
 render_router_paths(S, Rid) :-
   forall(path(Rid, Label, Cond, Nodes),
-    ( % abrimos el <li> de la ruta
-      format(S, "<li><strong>Ruta: ~w</strong>", [Label]),
+    ( format(S, "<li><strong>Ruta: ~w</strong>", [Label]),
       ( Cond == none ; Cond == [] -> true
-      ; format(S, " — <em>Condición:</em> ~w", [Cond]) ),
-      % hijos (nodos y/o sub-routers) DENTRO del mismo <li>
-      format(S, "<ul>~n", []),
+      ; format(S, " - <em>Condicion:</em> ~w", [Cond]) ),
+      format(S, "</li>~n<ul>~n", []),
       render_nodes_list(S, Nodes),
-      format(S, "</ul></li>~n", [])
+      format(S, "</ul>~n", [])
     )).
 
 render_nodes_list(_, []) :- !.
 render_nodes_list(S, [rid(SubRid)|T]) :- !,
   router(SubRid, Tp, Nm, Attrs),
-  % imprimimos el sub-router AQUÍ (como hijo de la ruta actual)
   router_li(S, SubRid, Tp, Nm, Attrs),
   render_nodes_list(S, T).
 render_nodes_list(S, [Id|T]) :-
